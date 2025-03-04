d74e7fb91afd2f1bd583498d1b25ee80cb9b8107
allanp93 <allanpasus@gmail.com>
2025-03-04 17:27:40 -0300

    Project created

---
+++ src/main/java/com/sinaitech/saintslist/SaintslistApplication.java
@@ -0,0 +1,13 @@
+package com.sinaitech.saintslist;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SaintslistApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SaintslistApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sinaitech/saintslist/SaintslistApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sinaitech.saintslist;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SaintslistApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
