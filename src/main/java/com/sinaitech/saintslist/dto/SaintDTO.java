4bc79c92980f11e0a6bf06c2c88fd6137d4c3909
allanp93 <allanpasus@gmail.com>
2025-04-07 10:12:09 -0300

    Database seed update

---
+++ src/main/java/com/sinaitech/saintslist/dto/SaintDTO.java
@@ -0,0 +1,17 @@
+package com.sinaitech.saintslist.dto;
+
+
+public class SaintDTO {
+	private Long id;
+	private String title;
+	
+
+	private Integer year;
+	private String genre;
+	private String platform;
+	private String imgUrl;
+	
+	private String shortDescription;
+	
+	private String longDescription;
+}
