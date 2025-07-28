47dd325486fee7b4490d1480f23e0168ff06d2d5
allanp93 <allanpasus@gmail.com>
2025-07-28 11:33:14 -0300

    GET game by id

--- src/main/java/com/sinaitech/saintslist/controllers/SaintController.java
+++ src/main/java/com/sinaitech/saintslist/controllers/SaintController.java
@@ -4,9 +4,12 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.sinaitech.saintslist.dto.SaintDTO;
 import com.sinaitech.saintslist.dto.SaintMinDTO;
 import com.sinaitech.saintslist.services.SaintService;
 
@@ -21,6 +24,12 @@ public class SaintController {
 	@GetMapping
 	public List<SaintMinDTO> findAll() {		
 		List<SaintMinDTO> result = saintService.findAll();
+		return result;
+	}
+	
+	@GetMapping(value = "/{id}")
+	public SaintDTO findByID(@PathVariable Long id) {		
+		SaintDTO result = saintService.findById(id);
 		return result;
 	}
 }
--- src/main/java/com/sinaitech/saintslist/services/SaintService.java
+++ src/main/java/com/sinaitech/saintslist/services/SaintService.java
@@ -1,13 +1,16 @@
-package com.sinaitech.saintslist.services;
+  package com.sinaitech.saintslist.services;
 
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
+import com.sinaitech.saintslist.dto.SaintDTO;
 import com.sinaitech.saintslist.dto.SaintMinDTO;
 import com.sinaitech.saintslist.entities.Saint;
 import com.sinaitech.saintslist.repositories.SaintRepository;
+
 
 @Service
 public class SaintService {
@@ -15,8 +18,16 @@ public class SaintService {
 	@Autowired
 	private SaintRepository saintRepository;
 	
+	@Transactional(readOnly = true)
 	public List<SaintMinDTO> findAll() {
 		List<Saint> result = saintRepository.findAll();
 		return result.stream().map(x -> new SaintMinDTO(x)).toList();
+	}
+	
+	@Transactional(readOnly = true)
+	public SaintDTO findById(Long id) {
+		Saint result = saintRepository.findById(id).get();
+		SaintDTO dto = new SaintDTO(result);
+		return dto;
 	}
 }
